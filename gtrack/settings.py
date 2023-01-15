b99f3065206321956c5bef6bd55369c3f9ab7788
NsikanPatrick <nsikanpatrick69@gmail.com>
2023-01-15 11:51:09 +0100

    Hiding configuration details

--- gtrack/settings.py
+++ gtrack/settings.py
@@ -144,11 +144,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 
 
-# SMTP CONFIGURATION
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'adaowonsikan69@gmail.com'  # official company mail
-EMAIL_HOST_PASSWORD = 'nsikan01'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
