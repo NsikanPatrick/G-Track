dc2a942b884d896df4e06fe5015e8f09b6fcd978
NsikanPatrick <nsikanpatrick69@gmail.com>
2023-10-02 11:35:13 +0200

    Working on Email Script

--- gtrack_app/views.py
+++ gtrack_app/views.py
@@ -652,6 +652,54 @@ def single_recipient(request, user_id):
     return render(request, 'admins_dashboard/email_notifications/single_recipient.html', {'debtor': debtor})
 
 
+# @login_required
+# def single_recipient_send_mail(request):
+#     if request.method == 'POST':
+#         email = request.POST.get("email")
+#         email_subject = request.POST.get("email_subject")
+#         email_message = request.POST.get("email_message")
+#         attachment = request.FILES.get('email_file')
+#         content_type = attachment.content_type if attachment else None
+
+#         # Validate form data
+#         if not email or not email_subject or not email_message:
+#             return HttpResponseBadRequest('Missing required fields')
+
+#         # Create email message
+#         email_msg = MIMEMultipart()
+#         email_msg['Subject'] = email_subject
+#         email_msg['To'] = email
+#         email_msg.attach(MIMEText(email_message))
+
+#         # Add attachment, if provided
+#         if attachment:
+#             file_data = attachment.read()
+#             if content_type.startswith('image/'):
+#                 image = MIMEImage(file_data)
+#                 image.add_header('Content-Disposition', 'attachment', filename='image.jpg')
+#                 email_msg.attach(image)
+#             elif content_type == 'application/pdf':
+#                 attachment_msg = MIMEApplication(file_data, _subtype='pdf')
+#                 attachment_msg.add_header('Content-Disposition', 'attachment', filename='document.pdf')
+#                 email_msg.attach(attachment_msg)
+#             elif content_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+#                 attachment_msg = MIMEApplication(file_data, _subtype='docx')
+#                 attachment_msg.add_header('Content-Disposition', 'attachment', filename='document.docx')
+#                 email_msg.attach(attachment_msg)
+#             else:
+#                 return HttpResponseBadRequest('Unsupported file type')
+
+#         # Send email
+#         email_sender = 'nsikanadaowo90@gmail.com'
+#         email_password = 'jduzadsuapibwahj'
+#         with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=ssl.create_default_context()) as smtp:
+#             smtp.login(email_sender, email_password)
+#             smtp.send_message(email_msg)
+
+#         messages.success(request, "Email notification was successfully sent")
+#         return redirect('debtors')
+
+
 @login_required
 def single_recipient_send_mail(request):
     if request.method == 'POST':
@@ -666,38 +714,51 @@ def single_recipient_send_mail(request):
             return HttpResponseBadRequest('Missing required fields')
 
         # Create email message
-        email_msg = MIMEMultipart()
-        email_msg['Subject'] = email_subject
-        email_msg['To'] = email
-        email_msg.attach(MIMEText(email_message))
-
-        # Add attachment, if provided
-        if attachment:
-            file_data = attachment.read()
-            if content_type.startswith('image/'):
-                image = MIMEImage(file_data)
-                image.add_header('Content-Disposition', 'attachment', filename='image.jpg')
-                email_msg.attach(image)
-            elif content_type == 'application/pdf':
-                attachment_msg = MIMEApplication(file_data, _subtype='pdf')
-                attachment_msg.add_header('Content-Disposition', 'attachment', filename='document.pdf')
-                email_msg.attach(attachment_msg)
-            elif content_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-                attachment_msg = MIMEApplication(file_data, _subtype='docx')
-                attachment_msg.add_header('Content-Disposition', 'attachment', filename='document.docx')
-                email_msg.attach(attachment_msg)
-            else:
-                return HttpResponseBadRequest('Unsupported file type')
-
-        # Send email
         email_sender = 'nsikanadaowo90@gmail.com'
         email_password = 'jduzadsuapibwahj'
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=ssl.create_default_context()) as smtp:
-            smtp.login(email_sender, email_password)
-            smtp.send_message(email_msg)
+        context = ssl.create_default_context()
 
-        messages.success(request, "Email notification was successfully sent")
-        return redirect('debtors')
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            try:
+                smtp.login(email_sender, email_password)
+
+                # Create email_msg
+                email_msg = MIMEMultipart()
+                email_msg['Subject'] = email_subject
+                email_msg['To'] = email
+                email_msg.attach(MIMEText(email_message))
+
+                # Add attachment, if provided
+                if attachment:
+                    file_data = attachment.read()
+
+                    if content_type.startswith('image/'):
+                        image = MIMEImage(file_data)
+                        image.add_header('Content-Disposition', 'attachment', filename='image.jpg')
+                        email_msg.attach(image)
+                    elif content_type == 'application/pdf':
+                        attachment_msg = MIMEApplication(file_data, _subtype='pdf')
+                        attachment_msg.add_header('Content-Disposition', 'attachment', filename='document.pdf')
+                        email_msg.attach(attachment_msg)
+                    elif content_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                        attachment_msg = MIMEApplication(file_data, _subtype='docx')
+                        attachment_msg.add_header('Content-Disposition', 'attachment', filename='document.docx')
+                        email_msg.attach(attachment_msg)
+                    else:
+                        return HttpResponseBadRequest('Unsupported file type')
+
+                # Send email
+                smtp.send_message(email_msg)
+                messages.success(request, "Email notification was successfully sent")
+                return redirect('debtors')
+
+            except Exception as e:
+                # Handle email sending errors (e.g., SMTPException, connection errors)
+                return HttpResponseBadRequest(f'Email sending error: {str(e)}')
+
+    # Handle GET requests or other HTTP methods
+    return HttpResponseBadRequest('Invalid request')
+
 
 
 
@@ -714,11 +775,75 @@ def multiple_recipients(request):
     # return render(request, "admins_dashboard/email_notifications/multiple_recipient.html")
 
 
+# @login_required
+# def multiple_recipient_send_mail(request):
+#     if request.method == 'POST':
+#         email_subject = request.POST["email_subject"]
+#         email_message = request.POST["email_message"]
+#         attachment = request.FILES.get('email_file')
+#         content_type = attachment.content_type if attachment else None
+
+#         # Validate form data
+#         if not email_subject or not email_message:
+#             return HttpResponseBadRequest('Missing required fields')
+
+#         # Create email message
+#         email_sender = 'nsikanadaowo90@gmail.com'
+#         email_password = 'jduzadsuapibwahj'
+#         context = ssl.create_default_context()
+
+#         with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+#             smtp.login(email_sender, email_password)
+
+#             # Get all debtors
+#             debtors = Debtor.objects.all()
+
+#             for debtor in debtors:
+#                 email = debtor.email
+
+#                 if email:
+#                     # Create a new email_msg object for each debtor
+#                     email_msg = MIMEMultipart()
+#                     email_msg['Subject'] = email_subject
+#                     email_msg['To'] = email
+#                     email_msg.attach(MIMEText(email_message))
+
+#                     # Add attachment, if provided
+#                     if attachment:
+#                         file_data = attachment.read()
+
+#                         if content_type.startswith('image/'):
+#                             image = MIMEImage(file_data)
+#                             image.add_header('Content-Disposition', 'attachment', filename='image.jpg')
+#                             email_msg.attach(image)
+#                         elif content_type == 'application/pdf':
+#                             attachment_msg = MIMEApplication(file_data, _subtype='pdf')
+#                             attachment_msg.add_header('Content-Disposition', 'attachment', filename='document.pdf')
+#                             email_msg.attach(attachment_msg)
+#                         elif content_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+#                             attachment_msg = MIMEApplication(file_data, _subtype='docx')
+#                             attachment_msg.add_header('Content-Disposition', 'attachment', filename='document.docx')
+#                             email_msg.attach(attachment_msg)
+#                         else:
+#                             return HttpResponseBadRequest('Unsupported file type')
+
+#                     # Send email to the current debtor
+#                     smtp.send_message(email_msg)
+
+#                     # Reset attachment for the next email if it exists
+#                     if attachment:
+#                         attachment.seek(0)
+
+#         messages.success(request, "Your email was sent")
+#         return redirect('debtors')
+
+
+
 @login_required
 def multiple_recipient_send_mail(request):
     if request.method == 'POST':
-        email_subject = request.POST["email_subject"]
-        email_message = request.POST["email_message"]
+        email_subject = request.POST.get("email_subject")
+        email_message = request.POST.get("email_message")
         attachment = request.FILES.get('email_file')
         content_type = attachment.content_type if attachment else None
 
@@ -767,7 +892,11 @@ def multiple_recipient_send_mail(request):
                             return HttpResponseBadRequest('Unsupported file type')
 
                     # Send email to the current debtor
-                    smtp.send_message(email_msg)
+                    try:
+                        smtp.send_message(email_msg)
+                    except Exception as e:
+                        # Handle email sending errors (e.g., SMTPException, connection errors)
+                        return HttpResponseBadRequest(f'Email sending error: {str(e)}')
 
                     # Reset attachment for the next email if it exists
                     if attachment:
@@ -775,6 +904,7 @@ def multiple_recipient_send_mail(request):
 
         messages.success(request, "Your email was sent")
         return redirect('debtors')
+
 
 
 # View functions for the client dashboard
